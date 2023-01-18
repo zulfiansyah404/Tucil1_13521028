3838e19cd99ff22c5956be32b1b72b521e479065
M Zulfiansyah Bayu Pratama <16521512@std.stei.itb.ac.id>
2023-01-18 22:31:15 +0700

    Membuat semua kemungkinan solusi

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,12 @@ char op[4] = {'+', '-', '*', '/'};
 // Fungsi cek apakah bilangan bisa dibagi
 bool isDivisible(int a, int b)
 {
-    if (b != 0) {
-        if (a % b == 0) {
+    if (b != 0)
+    {
+        cout << "\t\t\tb tidak 0" << endl;
+        if (a % b == 0)
+        {
+            cout << "\t\t\ta habis dibagi b" << endl;
             return true;
         }
     }
@@ -56,6 +60,7 @@ void cekValid(int ans, string s)
     {
         cnt++;
         jawaban.push_back(s);
+        cout << "\t\t\t" << s << endl;
     }
 }
 
@@ -66,9 +71,16 @@ void bfSecond_1(vector<int> card, string s)
     string tempS = s;
     vector<int> tempCard = card;
 
+    cout << "\tcard = ";
+    for (int i = 0; i < 3; i++)
+    {
+        cout << card[i] << " ";
+    }
+    cout << endl;
     // Operasikan kartu hasil operasi pertama dengan kartu ketiga
     for (int i = 0; i < 4; i++)
     {
+        cout << "\t\t\t" << card[0] << op[i] << card[1] << endl;
         // Kemungkinan semua operator
         if (i == 0)
         {
@@ -84,11 +96,14 @@ void bfSecond_1(vector<int> card, string s)
         }
         else if (i == 3)
         {
-            if (!isDivisible(card[0], card[1]));
+            if (isDivisible(card[0], card[1]) == false)
+                ;
             {
+                cout << "\t\t\tGagal" << endl;
                 break;
             }
             tempCard[1] = card[0] / card[1];
+            cout << "\t\t\tLolos" << endl;
         }
 
         tempCard.erase(tempCard.begin());
@@ -99,6 +114,7 @@ void bfSecond_1(vector<int> card, string s)
         // Sekarang operasikan kartu hasil operasi barusan dengan kartu keempat
         for (int j = 0; j < 4; j++)
         {
+
             if (j == 0)
             {
                 tempCard[0] = tempCard[0] + card[2];
@@ -113,7 +129,7 @@ void bfSecond_1(vector<int> card, string s)
             }
             else if (j == 3)
             {
-                if (!isDivisible(tempCard[0], card[2]))
+                if (isDivisible(tempCard[0], card[2]) == false)
                 {
                     break;
                 }
@@ -140,13 +156,13 @@ void bfSecond_2(vector<int> card, string s)
     // Buat string dan vector sementara
     string tempS;
     vector<int> tempCard = card;
-    cout << "\tcard = ";
-    for (int i = 0; i < 3; i++)
-    {
-        cout << card[i] << " ";
-    }
-    cout << endl;
-    cout << "\tstring = " << s << endl;
+    // cout << "\tcard = ";
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     cout << card[i] << " ";
+    // }
+    // cout << endl;
+    // cout << "\tstring = " << s << endl;
     // Operasikan kartu pertama dan kedua
     for (int i = 0; i < 4; i++)
     {
@@ -176,18 +192,18 @@ void bfSecond_2(vector<int> card, string s)
         tempS = "(" + to_string(card[0]) + op[i] + to_string(card[1]) + ")";
         vector<int> tempCard2 = tempCard;
 
-        cout << "\t\tstring2 = " << tempS << endl;
-        cout << "\t\tcard2 = ";
-        for (int i = 0; i < 2; i++)
-        {
-            cout << tempCard[i] << " ";
-        }
-        cout << endl;
+        // cout << "\t\tstring2 = " << tempS << endl;
+        // cout << "\t\tcard2 = ";
+        // for (int i = 0; i < 2; i++)
+        // {
+        //     cout << tempCard[i] << " ";
+        // }
+        // cout << endl;
         // Sekarang operasikan kartu hasil operasi barusan dengan operasi pertama
         for (int j = 0; j < 4; j++)
         {
-            cout << "\t\t\toperator = " << op[j] << endl;
-            cout << "\t\t\tcard[2] = " << card[2] << endl;
+            // cout << "\t\t\toperator = " << op[j] << endl;
+            // cout << "\t\t\tcard[2] = " << card[2] << endl;
             if (j == 0)
             {
                 tempCard[0] = tempCard[0] + card[2];
@@ -222,6 +238,164 @@ void bfSecond_2(vector<int> card, string s)
     }
 }
 
+void bfThird_1(vector<int> card, string s)
+{
+    string tempS;
+    vector<int> tempCard = card;
+
+    // Operasikan kartu pertama dengan hasil kartu dari operasi pertama lalu operasikan dengan kartu keempat
+    for (int i = 0; i < 4; i++)
+    {
+        // Kemungkinan semua operator
+        if (i == 0)
+        {
+            tempCard[1] = card[0] + card[1];
+        }
+        else if (i == 1)
+        {
+            tempCard[1] = card[0] - card[1];
+        }
+        else if (i == 2)
+        {
+            tempCard[1] = card[0] * card[1];
+        }
+        else if (i == 3)
+        {
+            if (isDivisible(card[0], card[1]) == false)
+            {
+                break;
+            }
+            tempCard[1] = card[0] / card[1];
+        }
+        // cout << "\t\toperator = " << op[i] << endl;
+        tempCard.erase(tempCard.begin());
+        tempS = "(" + to_string(card[0]) + op[i] + s + ")";
+        // cout << "\t\t" + tempS << endl;
+        vector<int> tempCard2 = tempCard;
+
+        // Sekarang operasikan kartu hasil operasi barusan dengan kartu keempat
+        for (int j = 0; j < 4; j++)
+        {
+            if (j == 0)
+            {
+                tempCard[0] = tempCard[0] + card[2];
+            }
+            else if (j == 1)
+            {
+                tempCard[0] = tempCard[0] - card[2];
+            }
+            else if (j == 2)
+            {
+                tempCard[0] = tempCard[0] * card[2];
+            }
+            else if (j == 3)
+            {
+                if (isDivisible(tempCard[0], card[2]) == false)
+                {
+                    break;
+                }
+                tempCard[0] = tempCard[0] / card[2];
+            }
+
+            tempS = tempS + op[j] + to_string(card[2]);
+            cekValid(tempCard[0], tempS);
+
+            // Kembalikan string dan tempCard ke semula
+            tempS = "(" + to_string(card[0]) + op[i] + s + ")";
+            tempCard = tempCard2;
+        }
+
+        // Kembalikan string dan tempCard ke semula
+        tempS = s;
+        tempCard = card;
+    }
+}
+
+void bfThird_2(vector<int> card, string s)
+{
+    string tempS;
+    vector<int> tempCard = card;
+
+    // Operasikan kartu keempat dengan hasil kartu dari operasi pertama lalu operasikan dengan kartu pertama
+    for (int i = 0; i < 4; i++)
+    {
+        // Kemungkinan semua operator
+        if (i == 0)
+        {
+            tempCard[1] = card[1] + card[2];
+        }
+        else if (i == 1)
+        {
+            tempCard[1] = card[1] - card[2];
+        }
+        else if (i == 2)
+        {
+            tempCard[1] = card[1] * card[2];
+        }
+        else if (i == 3)
+        {
+            if (isDivisible(card[1], card[2]) == false)
+            {
+                break;
+            }
+            tempCard[1] = card[1] / card[2];
+        }
+        // cout << "\t\toperator = " << op[i] << endl;
+        tempCard.erase(tempCard.begin() + 2);
+        tempS = "(" + s + op[i] + to_string(card[2]) + ")";
+        // cout << "\t\t" + tempS << endl;
+        vector<int> tempCard2 = tempCard;
+
+        // Sekarang operasikan kartu hasil operasi barusan dengan kartu pertama
+        for (int j = 0; j < 4; j++)
+        {
+            if (j == 0)
+            {
+                tempCard[0] = tempCard[1] + card[0];
+            }
+            else if (j == 1)
+            {
+                tempCard[0] = card[0] - tempCard[1];
+            }
+            else if (j == 2)
+            {
+                tempCard[0] = tempCard[1] * card[0];
+            }
+            else if (j == 3)
+            {
+                if (isDivisible(card[0], tempCard[1]) == false)
+                {
+                    break;
+                }
+                tempCard[0] = card[0] / tempCard[1];
+            }
+
+            tempS = to_string(card[0]) + op[j] + tempS;
+            cekValid(tempCard[0], tempS);
+
+            // Kembalikan string dan tempCard ke semula
+            tempS = "(" + s + op[i] + to_string(card[2]) + ")";
+            tempCard = tempCard2;
+        }
+
+        // Kembalikan string dan tempCard ke semula
+        tempS = s;
+        tempCard = card;
+    }
+}
+
+// Prosedur ketika kartu kedua dan ketiga sudah dioperasikan
+void bfSecond_3(vector<int> card, string s)
+{
+    // Ada dua cabang dalam operasi ini
+    // 1. Operasikan kartu pertama dengan hasil kartu dari operasi pertama lalu operasikan dengan kartu keempat
+    // 2. Operasikan kartu keempat dengan hasil kartu dari operasi pertama lalu operasikan dengan kartu pertama
+
+    // Operasi 1
+    bfThird_1(card, s);
+    bfThird_2(card, s);
+}
+
 // Prosedur untuk menentukan kartu mana yang harus dioperasikan terlebih dahulu
 void bfFirst(vector<int> card)
 {
@@ -232,7 +406,6 @@ void bfFirst(vector<int> card)
     {
 
         int j = i + 1;
-        cout << "i = " << i << " j = " << j << endl;
 
         for (int k = 0; k < 4; k++)
         {
@@ -258,24 +431,22 @@ void bfFirst(vector<int> card)
                 }
                 tempCard[j] = card[i] / card[j];
             }
-            cout << "\t"
-                 << "operasi = " << op[k] << endl;
+            // cout << "\t"
+            //      << "operasi = " << op[k] << endl;
             tempCard.erase(tempCard.begin() + i);
             s = "(" + to_string(card[i]) + op[k] + to_string(card[j]) + ")";
 
             if (i == 0)
             {
                 bfSecond_1(tempCard, s);
-                cout << "Sukes 1" << endl;
             }
             else if (i == 2)
             {
                 bfSecond_2(tempCard, s);
-                cout << "Sukes 2" << endl;
             }
             else
             {
-                // bfSecond_1(tempCard, s);
+                bfSecond_3(tempCard, s);
             }
 
             // Kembalikan string dan tempCard ke semula
@@ -303,6 +474,12 @@ int main()
 
     do
     {
+        cout << "card = ";
+        for (int i = 0; i < 4; i++)
+        {
+            cout << card[i] << " ";
+        }
+        cout << endl;
         bfFirst(card);
     } while (next_permutation(card.begin(), card.end()));
 
